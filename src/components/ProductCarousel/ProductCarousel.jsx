2ebb7110ac23346d0e89d932b6cde44822dec1bd
denisleonelortiz <denisleonelortiz@gmail.com>
2025-08-13 00:35:59 -0300

    change position navigation button

--- src/components/ProductCarousel/ProductCarousel.jsx
+++ src/components/ProductCarousel/ProductCarousel.jsx
@@ -24,6 +24,7 @@ export default function ProductCarousel({ images }) {
         style={{
           '--swiper-navigation-color': 'gray',
           '--swiper-pagination-color': '#000000ff',
+          '--swiper-navigation-sides-offset': '0px',
         }}
         pagination={{ clickable: true }}
         autoplay={{ delay: 5000 }}

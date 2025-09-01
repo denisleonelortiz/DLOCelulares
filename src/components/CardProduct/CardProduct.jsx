3180b770adde10db2049d0e3a14e455ff2339414
denisleonelortiz <denisleonelortiz@gmail.com>
2025-09-01 17:55:48 -0300

    fix hover in movile

--- src/components/CardProduct/CardProduct.jsx
+++ src/components/CardProduct/CardProduct.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState, useRef } from "react"
+import { useContext, useEffect, useState } from "react"
 import styles from "./CardProduct.module.css"
 import { Link } from "react-router-dom"
 import { CartContext } from "../../context/CartContext"
@@ -10,27 +10,13 @@ export default function CardProduct({ product }) {
     const [quantity, setQuantity] = useState(0)
     const { carrito } = useContext(CartContext)
     const { handlerAddButton, handlerIncrease, handlerDecrease } = useContext(CardProductContext)
-    const addBtnRef = useRef(null)
-    const [showAddBtn, setShowAddBtn] = useState(true);
 
     useEffect(() => {
         let newQuantity = carrito.find(item => item.id === product.id)?.quantity || 0
-
+        
         setQuantity(newQuantity)
     }, [carrito])
 
-    useEffect(() => {
-        if (addBtnRef.current) {
-            addBtnRef.current.blur(); // quita el estado activo del botón al renderizar
-        }
-    }, [quantity]);
-
-
-    const handleDecrease = () => {
-        handlerDecrease(product);
-        setShowAddBtn(false);
-        setTimeout(() => setShowAddBtn(true), 50); // 50ms de retraso evita el pseudo-hover
-    };
 
 
     return (
@@ -45,10 +31,10 @@ export default function CardProduct({ product }) {
 
             <div className={styles.btnContainer}>
                 {quantity === 0 ?
-                    <button className={styles.button} onClick={() => handlerAddButton(product)} ref={addBtnRef} >Agregar al carrito</button>
+                    <button className={styles.button} onClick={() => handlerAddButton(product)} >Agregar al carrito</button>
 
                     : <>
-                        <button className={`btn px-3 ${styles.btnRestSum}`} onClick={() => handleDecrease(product)} ref={addBtnRef}>-</button>
+                        <button className={`btn px-3 ${styles.btnRestSum}`} onClick={() => handlerDecrease(product)}>-</button>
                         <span className=" m-auto">{quantity} en tu carrito</span>
                         <button className={`btn px-3 ${styles.btnRestSum}`} onClick={() => handlerIncrease(product)}>+</button>
                     </>
